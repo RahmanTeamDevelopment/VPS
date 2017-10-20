3cbedb2e58e505d1e5b4dedd319e592ac8bafea5
Márton Münz <Marton.Muenz@icr.ac.uk>
2017-10-20 02:38:43 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = '...',
+    version = '0.1.0',
+    description = '...',
+    url = '...',
+    author = 'Marton Munz',
+    author_email = 'munzmarci@gmail.com',
+    license = 'MIT',
+    packages=[ ],
+    scripts=[ ],
+    zip_safe=False
+)
